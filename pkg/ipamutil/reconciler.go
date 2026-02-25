69a062144fa6bd31fde607f4c66b35e781cbdc49
Mohamed Chiheb Ben Jemaa <mc.benjemaa@gmail.com>
2026-02-25 15:39:21 +0100

    feat: Add spec.addressReuseGracePeriodSeconds in IPPool to control the time before releasing the IPs

--- pkg/ipamutil/reconciler.go
+++ pkg/ipamutil/reconciler.go
@@ -278,8 +278,11 @@ func (r *ClaimReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ct
 }
 
 func (r *ClaimReconciler) reconcileDelete(ctx context.Context, claim *ipamv1.IPAddressClaim, handler ClaimHandler) (ctrl.Result, error) {
-	if res, err := handler.ReleaseAddress(ctx); err != nil {
-		return unwrapResult(res), fmt.Errorf("release address: %w", err)
+	if res, err := handler.ReleaseAddress(ctx); err != nil || res != nil {
+		if err != nil {
+			err = fmt.Errorf("release address: %w", err)
+		}
+		return unwrapResult(res), err
 	}
 
 	address := &ipamv1.IPAddress{}
